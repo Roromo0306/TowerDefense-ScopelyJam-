235ab2eec0ec94e4f155ea8880e5dbae5e2c93d2
Alonso2880 <40578797+Alonso2880@users.noreply.github.com>
2025-10-25 11:33:39 +0200

    .

--- TowerDefenseScopely/Assets/Scripts/GridManager.cs
+++ TowerDefenseScopely/Assets/Scripts/GridManager.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] private int _width, _height;
-    [SerializeField] private Tile tilePrefab;
 
-
-    private void Start()
-    {
-        GenerateGrid();
-    }
-    void GenerateGrid()
-    {
-        for(int x = 0; x < _width; x++)
-        {
-            for(int y = 0; y < _height; y++)
-            {
-                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
-                spawnedTile.name = $"Tile {x} {y}";
-            }
-        }
-    }
 }
