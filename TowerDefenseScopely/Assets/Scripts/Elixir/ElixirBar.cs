0804ffbfe257ffaddd6e558e5c52311120204ab8
Roromo0306 <pownyrainbow03@gmail.com>
2025-10-25 12:34:21 +0200

    lolazo

---
+++ TowerDefenseScopely/Assets/Scripts/Elixir/ElixirBar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ElixirBar : MonoBehaviour
+{
+    [Header("UI")]
+    public Slider elixirSlider;
+    public TextMeshProUGUI elixirText;
+
+    [Header("Elixir Settings")]
+    public float maxElixir = 10f;
+    public float currentElixir = 0f;
+    public float cargaElixir = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elixirSlider.maxValue= maxElixir;
+        elixirSlider.value = currentElixir;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RegenerarElixir();
+      
+    }
+
+    void RegenerarElixir()
+    {
+        if (currentElixir < maxElixir)
+        {
+            currentElixir += cargaElixir * Time.deltaTime;
+        }
+    }
+}
