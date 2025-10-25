5084e2d7fc8dfb9277c84787e0d1a96aabe9b8fd
Roromo0306 <pownyrainbow03@gmail.com>
2025-10-25 11:54:03 +0200

    enemigos parte 34254

---
+++ TowerDefenseScopely/Assets/Scripts/Enemigos/Herencia/Esbirros.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esbirros : Enemy
+{
+  protected void MoveTowardsTarget()
+    {
+        base.MoveTowardsTarget();   
+    }
+}
---
+++ TowerDefenseScopely/Assets/Scripts/Enemigos/ManagerEnemigos.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerEnemigos : MonoBehaviour
+{
+    public Enemy[] enemyPrefabs;
+    public Transform[] spawnPoints;
+    public float SpawnInterval;
+
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= SpawnInterval)
+        {
+            SpawnEnemy();
+            timer = 0;  
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        if(enemyPrefabs.Length == 0 ||  spawnPoints.Length == 0)
+        {
+            return;
+        }
+
+        Enemy enemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+        Transform transform = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        Instantiate(enemy, transform.transform);
+    }
+}
