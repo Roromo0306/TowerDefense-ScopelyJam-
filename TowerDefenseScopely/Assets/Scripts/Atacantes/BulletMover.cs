cc1890f9a548b56dfd2bf9476e5b1943300b90aa
Alonso2880 <40578797+Alonso2880@users.noreply.github.com>
2025-10-26 02:16:31 +0200

    personajes rojos cuando se golpean. YIHAAAAAAAAAAAAAAAAAA

--- TowerDefenseScopely/Assets/Scripts/Atacantes/BulletMover.cs
+++ TowerDefenseScopely/Assets/Scripts/Atacantes/BulletMover.cs
@@ -7,27 +7,42 @@ public class BulletMover : MonoBehaviour
     public Vector3 direction;
     public float speed = 8f;
 
+    public SpriteRenderer rend;
+
+    public float flashDuration = 0.5f;
+
     void Update()
     {
         transform.position += direction * speed * Time.deltaTime;
     }
 
-    void Start()
-    {
-
-    }
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemigo"))
         {
-            Destroy(this.gameObject);
+            if (collision.TryGetComponent<SpriteRenderer>(out SpriteRenderer rend))
+            {
+                StartCoroutine(FlashAndDestroy(rend, Color.red, flashDuration));
+            }
         }
     }
-}
+
+    private IEnumerator FlashAndDestroy(SpriteRenderer rend, Color flashColor, float duration)
+    {
+
+        Color original = rend.color;
+        rend.color = flashColor;
+
+        yield return new WaitForSeconds(duration);
+
+ 
+        if (rend != null)
+        {
+            rend.color = original;
+        }
+
+        Destroy(this.gameObject);
+    }
+    }
+
